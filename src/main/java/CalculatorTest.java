4d60bc942c9a1a679d91616fb91bba7b2b75407c
Celina Urwank <celina.urwank@swisscom.com>
2018-11-30 08:56:01 +0100

    implements the test method for test positive. not fully working yet.

--- src/main/java/CalculatorTest.java
+++ src/main/java/CalculatorTest.java
@@ -4,6 +4,19 @@ CalculatorTest Class
 @version 16.11.2018
  */
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
 public class CalculatorTest {
+
+    Calculator testE;
+
+    @Test
+    public void testPositive() {
+        testE = new Calculator();
+        assertTrue(testE.addition(10, 25) == 35);
+    }
 
 }
