e22f597ba6169298c1935f4f6494a566ae665c57
Celina Urwank <celina.urwank@swisscom.com>
2018-12-07 08:51:04 +0100

    implemented junit tests for subtraction methods

--- src/main/java/CalculatorTest.java
+++ src/main/java/CalculatorTest.java
@@ -13,16 +13,36 @@ public class CalculatorTest {
 
     Calculator testE;
 
+    /*
+    ========================
+    ===== TEST METHODS =====
+    ========================
+     */
+
+    // addition methods
     @Test
-    public void testPositive() {
+    public void testPositiveAdd() {
         testE = new Calculator();
         assertTrue(testE.addition(10, 25) == 35);
     }
 
     @Test
-    public void testNegative() {
+    public void testNegativeAdd() {
         testE = new Calculator();
         assertTrue(testE.addition(-10, -25) == -35);
+    }
+
+    // subtraction methods
+    @Test
+    public void testPositiveSub() {
+        testE = new Calculator();
+        assertTrue(testE.subtraction(10, 5) == 5);
+    }
+
+    @Test
+    public void testNegativeSub() {
+        testE = new Calculator();
+        assertTrue(testE.subtraction(-10, -5) == -5);
     }
 
 }
